0579127020aa9597be5d4cad97ff656c3c16227b
Gregor Faiman <faiman@fotec.at>
2020-12-03 19:45:46 +0100

    SudokuSolverUnit Tests

--- SudokuSolverTests/SudokuSolverTests.cs
+++ SudokuSolverTests/SudokuSolverTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using SudokuSolver_WPF.BusinessLogic;
@@ -27,22 +28,129 @@ namespace SudokuSolverTests
                     0, 6, 0, 0, 0, 0, 2, 8, 0
                     0, 0, 0, 4, 1, 9, 0, 0, 5
                     0, 0, 0, 0, 8, 0, 0, 7, 9")]
+        [TestCase(@"0,2,0,0,0,8,7,0,0
+                    0,0,0,5,0,0,0,4,3
+                    0,6,3,0,4,0,0,0,0
+                    3,0,0,0,0,0,2,0,0
+                    0,0,0,0,0,0,0,0,0
+                    5,0,7,0,0,4,0,0,0
+                    0,0,0,4,0,2,0,0,8
+                    0,3,1,9,7,5,0,0,0
+                    0,5,4,3,0,6,0,9,7")]
+
         public void Does_Sudoku_GetCreated_From_Valid_Input(string input)
         {
             var cells = this.parser.Parse(input);
-            var splitInput = input.Split('\r', '\n').Where(p => !string.IsNullOrWhiteSpace(p));
 
-            if (cells.Length == splitInput.Count())
+            var splitInput = input.Split('\r', '\n').Where(p => !string.IsNullOrWhiteSpace(p));
+            var control = new List<string>();
+
+            foreach (var item in splitInput)
             {
-                for (int i = 0; i < cells.Length; i++)
-                {
-                    Assert.That(cells[i].Content == Convert.ToInt32(splitInput.ElementAt(i)));
-                }
+                control.AddRange(item.Replace(" ", string.Empty).Split(','));
             }
-            else
+
+            if (control.Count != cells.Length)
+                Assert.Fail();
+
+            for (int i = 0; i < cells.Length; i++)
             {
-                Assert.Fail("Cells and split input were not same length");
+                if (cells[i].Content != int.Parse(control[i]))
+                    Assert.Fail();
             }
+
+            Assert.Pass();
+        }
+
+        [Test]
+        [TestCase(@"5, 3, 5, 0, 7, 0, 0, 0, 0
+                    6, 0, 0, 1, 9, 5, 0, 0, 0
+                    0, 9, 8, 0, 0, 0, 0, 6, 0
+                    8, 0, 0, 0, 6, 0, 0, 0, 3
+                    4, 0, 0, 8, 0, 3, 0, 0, 1
+                    7, 0, 0, 0, 2, 0, 0, 0, 6
+                    0, 6, 0, 0, 0, 0, 2, 8, 0
+                    0, 0, 0, 4, 1, 9, 0, 0, 5
+                    0, 3, 0, 0, 8, 0, 0, 7, 9")]
+        [TestCase(@"2, 0, 0, 0, 0, 8, 7, 0, 0
+                    0, 0, 0, 5, 0, 0, 0, 4, 3
+                    0, 6, 3, 0, 4, 0, 0, 0, 0
+                    3, 0, 0, 0, 0, 0, 2, 0, 0
+                    0, 0, 0, 0, 0, 0, 0, 0, 0
+                    5, 0, 7, 0, 0, 4, 0, 0, 0
+                    0, 0, 0, 4, 0, 2, 0, 0, 8
+                    0, 3, 1, 9, 7, 5, 0, 0, 0
+                    0, 0, 4, 3, 0, 6, 0, 9, 7")]
+
+        public void Does_Solve_On_Invalid_Sudoku_ReturnFalse(string input)
+        {
+            var cells = this.parser.Parse(input);
+
+            var isValid = this.solver.TrySolve(cells);
+
+            Assert.False(isValid);
+        }
+
+        [Test]
+        [TestCase(@"5, 3, 0, 0, 7, 0, 0, 0, 0
+                    6, 0, 0, 1, 9, 5, 0, 0, 0
+                    0, 9, 8, 0, 0, 0, 0, 6, 0
+                    8, 0, 0, 0, 6, 0, 0, 0, 3
+                    4, 0, 0, 8, 0, 3, 0, 0, 1
+                    7, 0, 0, 0, 2, 0, 0, 0, 6
+                    0, 6, 0, 0, 0, 0, 2, 8, 0
+                    0, 0, 0, 4, 1, 9, 0, 0, 5
+                    0, 0, 0, 0, 8, 0, 0, 7, 9")]
+        [TestCase(@"0,2,0,0,0,8,7,0,0
+                    0,0,0,5,0,0,0,4,3
+                    0,6,3,0,4,0,0,0,0
+                    3,0,0,0,0,0,2,0,0
+                    0,0,0,0,0,0,0,0,0
+                    5,0,7,0,0,4,0,0,0
+                    0,0,0,4,0,2,0,0,8
+                    0,3,1,9,7,5,0,0,0
+                    0,5,4,3,0,6,0,9,7")]
+        public void Does_Solve_On_Valid_Sudoku_ReturnTrue(string input)
+        {
+            var cells = this.parser.Parse(input);
+
+            var isValid = this.solver.TrySolve(cells);
+
+            Assert.True(isValid);
+        }
+
+        [Test]
+        [TestCase(@"5, 3, 0, 0, 7, 0, 0, 0, 0
+                    6, 0, 0, 1, 9, 5, 0, 0, 0
+                    0, 9, 8, 0, 0, 0, 0, 6, 0
+                    8, 0, 0, 0, 6, 0, 0, 0, 3
+                    4, 0, 0, 8, 0, 3, 0, 0, 1
+                    7, 0, 0, 0, 2, 0, 0, 0, 6
+                    0, 6, 0, 0, 0, 0, 2, 8, 0
+                    0, 0, 0, 4, 1, 9, 0, 0, 5
+                    0, 0, 0, 0, 8, 0, 0, 7, 9")]
+        [TestCase(@"0,2,0,0,0,8,7,0,0
+                    0,0,0,5,0,0,0,4,3
+                    0,6,3,0,4,0,0,0,0
+                    3,0,0,0,0,0,2,0,0
+                    0,0,0,0,0,0,0,0,0
+                    5,0,7,0,0,4,0,0,0
+                    0,0,0,4,0,2,0,0,8
+                    0,3,1,9,7,5,0,0,0
+                    0,5,4,3,0,6,0,9,7")]
+        public void Does_Solve_On_Valid_Sudoku_SolveItCorrectly(string input)
+        {
+            var cells = this.parser.Parse(input);
+
+            this.solver.TrySolve(cells);
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (cells.Count(cell => cell.Content == i) != 9)
+                    Assert.Fail();
+            }
+
+            Assert.Pass();
         }
     }
 }
